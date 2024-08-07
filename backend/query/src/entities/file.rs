4b618f091126c55e98ac3c44695531daa7afc9d5
mikeshinoda <53910940+desonglll@users.noreply.github.com>
2024-08-07 16:09:18 +0800

    Update

--- backend/query/src/entities/file.rs
+++ backend/query/src/entities/file.rs
@@ -2,20 +2,20 @@ use std::fs;
 use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
 
-use actix_multipart::form::MultipartForm;
 use actix_multipart::form::tempfile::TempFile;
 use actix_multipart::form::text::Text;
+use actix_multipart::form::MultipartForm;
 use actix_session::Session;
 use chrono::{Local, NaiveDateTime};
-use diesel::{Insertable, Queryable, RunQueryDsl, Selectable, SelectableHelper};
 use diesel::dsl::insert_into;
+use diesel::{Insertable, Queryable, RunQueryDsl, Selectable, SelectableHelper};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 use uuid::Uuid;
 
 use shared::lib::data::Data;
 
-use crate::{DbPool, establish_pg_connection};
+use crate::{establish_pg_connection, DbPool};
 
 #[derive(Debug, MultipartForm)]
 pub struct UploadForm {
@@ -52,12 +52,19 @@ impl File {
     pub fn new(session: &Session, upload_form: &mut UploadForm) -> Self {
         let user_id = session.get::<i32>("user_id").unwrap().unwrap_or(-1);
         let description = upload_form.description.clone().clone();
-        let base_url: String = std::env::current_dir().unwrap().to_str().unwrap().to_string();
+        let base_url: String = std::env::current_dir()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string();
         let upload_folder = format!("{}/upload", base_url);
         Self {
             id: Uuid::new_v4(),
             name: upload_form.file.file_name.clone().unwrap(),
-            path: File::get_save_path(upload_folder, &upload_form.file).to_str().unwrap().to_string(),
+            path: File::get_save_path(upload_folder, &upload_form.file)
+                .to_str()
+                .unwrap()
+                .to_string(),
             size: upload_form.file.size as i64,
             content_type: upload_form.file.content_type.clone().unwrap().to_string(),
             created_at: Some(Local::now().naive_local()),
@@ -69,8 +76,12 @@ impl File {
         }
     }
 
-    pub fn save(&self, pool: &DbPool, session: &Session, mut upload_form: UploadForm) -> Result<Data<File>, Box<dyn std::error::Error>
-    > {
+    pub fn save(
+        &self,
+        pool: &DbPool,
+        session: &Session,
+        mut upload_form: UploadForm,
+    ) -> Result<Data<File>, Box<dyn std::error::Error>> {
         let mut conn = establish_pg_connection(pool).unwrap();
         // 获取保存路径
         let save_path = Path::new(&self.path);
@@ -85,7 +96,7 @@ impl File {
 
         // 从 NamedTempFile 读取内容并写入到目标文件
         let mut temp_file = upload_form.file.file.as_file(); // 获取文件的可读引用
-        // 重置文件指针到文件开头
+                                                             // 重置文件指针到文件开头
         temp_file.seek(SeekFrom::Start(0))?;
         let mut buffer = Vec::new();
         let bytes_read = temp_file.read_to_end(&mut buffer)?; // 读取临时文件内容到缓冲区
@@ -124,108 +135,5 @@ impl File {
         let mut path = PathBuf::from(base_path);
         path.push(file_name);
         path
-    }
-}
-
-#[cfg(test)]
-mod file_tests {
-    use std::fs::File;
-
-    use actix_web::http::header::{ContentDisposition, DispositionParam, DispositionType};
-
-    use super::*;
-
-    #[test]
-    fn test_get_filename_with_valid_filename() {
-        // 构建一个包含有效文件名的 ContentDisposition
-        let content_disposition = ContentDisposition {
-            disposition: DispositionType::Attachment,
-            parameters: vec![DispositionParam::Filename(String::from("test_file.txt"))],
-        };
-
-        // 调用 get_filename 函数
-        let filename = File::get_filename(&content_disposition);
-
-        // 断言文件名是我们期望的文件名
-        assert_eq!(filename, "test_file.txt");
-    }
-
-    #[test]
-    fn test_get_filename_with_missing_filename() {
-        // 构建一个不包含文件名的 ContentDisposition
-        let content_disposition = ContentDisposition {
-            disposition: DispositionType::Attachment,
-            parameters: vec![], // 没有 Filename 参数
-        };
-
-        // 调用 get_filename 函数
-        let filename = File::get_filename(&content_disposition);
-
-        // 断言文件名是默认文件名
-        assert_eq!(filename, "default_filename");
-    }
-
-    #[test]
-    fn test_get_file_type_with_valid_filename() {
-        // 构建一个包含有效文件名的 ContentDisposition
-        let content_disposition = ContentDisposition {
-            disposition: DispositionType::Attachment,
-            parameters: vec![DispositionParam::Filename(String::from("test_file.txt"))],
-        };
-
-        // 调用 get_file_type 函数
-        let file_type = File::get_file_type(&content_disposition);
-
-        // 断言文件类型是我们期望的文件类型
-        assert_eq!(file_type, "test_file.txt");
-    }
-
-    #[test]
-    fn test_get_file_type_with_missing_filename() {
-        // 构建一个不包含文件名的 ContentDisposition
-        let content_disposition = ContentDisposition {
-            disposition: DispositionType::Attachment,
-            parameters: vec![], // 没有 Filename 参数
-        };
-
-        // 调用 get_file_type 函数
-        let file_type = File::get_file_type(&content_disposition);
-
-        // 断言文件类型是 "unknown"
-        assert_eq!(file_type, "unknown");
-    }
-
-    #[test]
-    fn test_calculate_hash() {
-        use std::io::Write;
-        // 创建一个临时文件并写入数据
-        let temp_file_path = PathBuf::from("./temp_file.txt");
-        let mut file = File::create(&temp_file_path).unwrap();
-        writeln!(file, "Hello, world!").unwrap();
-
-        // 调用 calculate_hash 函数
-        let hash = File::calculate_hash(&temp_file_path);
-
-        // 断言哈希值是我们期望的值
-        let expected_hash = "d9014c4624844aa5bac314773d6b689ad467fa4e1d1a50a1b8a99d5a95f72ff5";
-        assert_eq!(hash, expected_hash);
-
-        // 删除临时文件
-        fs::remove_file(temp_file_path).unwrap();
-    }
-
-    #[test]
-    fn test_get_save_path() {
-        // 调用 get_save_path 函数
-        let filename = "test_file.txt";
-        let base_url: String = std::env::var("CRAB_ROCKET_WORKING_DIR").unwrap();
-        let save_path = File::get_save_path(base_url.clone(), filename);
-
-        // 构造期望的路径
-        let mut expected_path = PathBuf::from(base_url.clone());
-        expected_path.push(filename);
-
-        // 断言生成的路径是我们期望的路径
-        assert_eq!(save_path, expected_path);
     }
 }
--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -1,7 +1,3 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::sync::Arc;
-
 use actix_cors::Cors;
 use actix_session::storage::RedisActorSessionStore;
 use actix_session::SessionMiddleware;
